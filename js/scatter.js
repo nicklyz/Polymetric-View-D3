08e3aaad65b48551436142bddfb56e2045cfc625
Yanzhe Liu <nickliuyanzhe@gmail.com>
2017-06-07 19:52:19 -0700

    Added redraw

--- js/scatter.js
+++ js/scatter.js
@@ -1,67 +1,11 @@
-var example = [
-  {"id": "1", "parent": "", "name": "Object",
-    "metric": {
-      "NOA": 1,
-      "NOM": 1,
-      "WLOC": 3,
-      "NOPA": 11,
-      "CC": 1,
-    }
-  },
-  {"id": "2", "parent": "1", "name": "String",
-    "metric": {
-      "NOA": 2,
-      "NOM": 3,
-      "WLOC": 5,
-      "NOPA": 9,
-      "CC": 5,
-    }
-  },
-  {"id": "3", "parent": "1", "name": "List",
-    "metric": {
-      "NOA": 5,
-      "NOM": 7,
-      "WLOC": 8,
-      "NOPA": 3,
-      "CC": 10,
-
-    }
-  },
-  {"id": "4", "parent": "3", "name": "LinkedList",
-    "metric": {
-      "NOA": 7,
-      "NOM": 9,
-      "WLOC": 10,
-      "NOPA": 4,
-      "CC": 15,
-    }
-  },
-  {"id": "5", "parent": "3", "name": "ArrayList",
-    "metric": {
-      "NOA": 9,
-      "NOM": 13,
-      "WLOC": 12,
-      "NOPA": 4,
-      "CC": 20,
-    }
-  },
-  {"id": "6", "parent": "", "name": "NULL",
-    "metric": {
-      "NOA": 12,
-      "NOM": 17,
-      "WLOC": 19,
-      "NOPA": 4,
-      "CC": 25,
-    }
-  }
-];
-
-function scatter_plot(example){
+function scatter(data){
 
     var w = 500,
         h = 500,
         pad = 20,
         left_pad = 100;
+
+    d3.selectAll("svg").remove();
 
     var svg = d3.select("#body")
             .append("svg")
@@ -93,28 +37,28 @@ function scatter_plot(example){
         .attr("x", function () { return w/2; })
         .attr("y", function () { return h/2-5; });
 
-    var max_h = d3.max(example.map(
+    var max_h = d3.max(data.map(
                            function (d) { return d.metric.NOA; })),
             hscale = d3.scale.linear()
-                .domain([0, d3.max(example, function (d) { return PMV.getMetric(d, "NOA"); })])
+                .domain([0, d3.max(data, function (d) { return PMV.getMetric(d, "NOA"); })])
                 .range([0, 50]);
 
-    var max_w = d3.max(example.map(
+    var max_w = d3.max(data.map(
                            function (d) { return PMV.getMetric(d, "NOPA"); })),
             wscale = d3.scale.linear()
-                .domain([0, d3.max(example, function (d) { return PMV.getMetric(d, "NOPA"); })])
+                .domain([0, d3.max(data, function (d) { return PMV.getMetric(d, "NOPA"); })])
                 .range([0, 50]);
 
 
     var fscale = d3.scale.linear()
-            .domain([0, d3.max(example, function (d) { return PMV.getMetric(d, "CC"); })])
+            .domain([0, d3.max(data, function (d) { return PMV.getMetric(d, "CC"); })])
             .range([100,0]);
 
 
     svg.selectAll(".loading").remove();
 
     svg.selectAll("rect")
-        .data(example)
+        .data(data)
         .enter()
         .append("rect")
         .attr("x", function (d) { return x(PMV.getMetric(d, "NOM")); })
@@ -124,9 +68,5 @@ function scatter_plot(example){
         .attr("width", function (d) { return wscale(PMV.getMetric(d, "NOPA")); })
         .attr("height", function (d) { return hscale(PMV.getMetric(d, "NOA")); })
         //.attr("shape-rendering", "crispEdges")
-        .style("fill", function(d) { return "hsl(200, 80%, " + fscale(PMV.getMetric(d, "CC")) + "%)" })
-
-
+        .style("fill", function(d) { return "hsl(200, 80%, " + fscale(PMV.getMetric(d, "CC")) + "%)" });
 }
-
-scatter_plot(example)
--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function parseFile() {
 
 		reader.onload = function(e) {
 			//fileDisplayArea.innerText = reader.result;	// replace the following with interesting logic
-			
+
 			var result = MSE.parse(reader.result);
 			var printData = JSON.stringify(result.slice(0,10), null, 2);
 			redraw();
@@ -26,7 +26,7 @@ function parseFile() {
 			fileDisplayArea.innerText = printData;
 		}
 
-		reader.readAsText(file);	
+		reader.readAsText(file);
 	} else {
 		fileDisplayArea.innerText = 'Please upload an MSE file!';
 	}
@@ -72,4 +72,78 @@ function adjustDimsAndRedraw() {
 
 function redraw() {
 	// add plotting mechanisms here
+	var example = [
+	  {"id": "1", "parent": "", "name": "Object",
+	    "metric": {
+	      "NOA": 1,
+	      "NOM": 1,
+	      "WLOC": 3,
+	      "NOPA": 11,
+	      "CC": 1,
+	    }
+	  },
+	  {"id": "2", "parent": "1", "name": "String",
+	    "metric": {
+	      "NOA": 2,
+	      "NOM": 3,
+	      "WLOC": 5,
+	      "NOPA": 9,
+	      "CC": 5,
+	    }
+	  },
+	  {"id": "3", "parent": "1", "name": "List",
+	    "metric": {
+	      "NOA": 5,
+	      "NOM": 7,
+	      "WLOC": 8,
+	      "NOPA": 3,
+	      "CC": 10,
+
+	    }
+	  },
+	  {"id": "4", "parent": "3", "name": "LinkedList",
+	    "metric": {
+	      "NOA": 7,
+	      "NOM": 9,
+	      "WLOC": 10,
+	      "NOPA": 4,
+	      "CC": 15,
+	    }
+	  },
+	  {"id": "5", "parent": "3", "name": "ArrayList",
+	    "metric": {
+	      "NOA": 9,
+	      "NOM": 13,
+	      "WLOC": 12,
+	      "NOPA": 4,
+	      "CC": 20,
+	    }
+	  },
+	  {"id": "6", "parent": "", "name": "NULL",
+	    "metric": {
+	      "NOA": 12,
+	      "NOM": 17,
+	      "WLOC": 19,
+	      "NOPA": 4,
+	      "CC": 25,
+	    }
+	  }
+	];
+
+	var layout = $('#layout').find(':selected').attr('value');
+
+	switch(layout) {
+		case 'scatter':
+			scatter(example);
+			break;
+		case 'tree':
+			tree(example);
+			break;
+		case 'treemap':
+			treemap(example);
+			break;
+		default:
+	}
 }
+
+redraw();
